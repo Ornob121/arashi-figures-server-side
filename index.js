17c017db12a61fff434d7e5f319d9a0195168aa1
Ornob121 <shadmantahmid121@gmail.com>
2023-05-20 09:56:49 +0600

    delete api created

--- index.js
+++ index.js
@@ -117,6 +117,14 @@ async function run() {
       res.send(result);
     });
 
+    //! Delete api
+    app.delete("/deleteToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await allToysCollection.deleteOne(filter);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
